2e0b56220179abc02067dc314ae22c70ea7c3eb7
CarlosDelvasto <est.carlos.delvasto@unimilitar.edu.co>
2021-06-04 04:50:09 -0500

    ya casi

--- TDIfinal/files.js
+++ TDIfinal/files.js
@@ -2,9 +2,13 @@ const db = firebase.firestore();
 
 const taskForm = document.getElementById("task-form");
 const tasksContainer = document.getElementById("tasks-container");
+const imgcarlos = document.getElementById('imgjulieta');
+const storageRef = firebase.storage().ref();
+
 
 let editStatus = false;
 let id = '';
+var seguimiento=[];
 
 /**
  * Save a New Task in Firestore
@@ -30,9 +34,11 @@ const updateTask = (id, updatedTask) => db.collection('tasks').doc(id).update(up
 window.addEventListener("DOMContentLoaded", async (e) => {
   onGetTasks((querySnapshot) => {
     tasksContainer.innerHTML = "";
+    var felipe = 0;
 
     querySnapshot.forEach((doc) => {
       const task = doc.data();
+      seguimiento[felipe] = task.URL;
 
       tasksContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
     <h3 class="h5">${task.title}</h3>
@@ -44,6 +50,12 @@ window.addEventListener("DOMContentLoaded", async (e) => {
       <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
         🖉 Edit
       </button>
+       
+        </div>
+      <div class="col-12">
+      <br>
+                            <img src=${task.URL} class="col-12" width="600px" alt="img">
+                        </div>
     </div>
   </div>`;
     });
@@ -81,29 +93,27 @@ window.addEventListener("DOMContentLoaded", async (e) => {
   });
 });
 
-taskForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const title = taskForm["task-title"];
-  const description = taskForm["task-description"];
-
-  try {
-    if (!editStatus) {
-      await saveTask(title.value, description.value);
-    } else {
-      await updateTask(id, {
-        title: title.value,
-        description: description.value,
-      })
-
-      editStatus = false;
-      id = '';
-      taskForm['btn-task-form'].innerText = 'Save';
-    }
-
-    taskForm.reset();
-    title.focus();
-  } catch (error) {
-    console.log(error);
-  }
-});
+taskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    var title = taskForm['task-title'].value;
+    var file = taskForm['photo'].files[0];
+    var description = taskForm['task-description'].value;
+    var imgnew = file.name;
+    var URL = '';
+    const imageRef = storageRef.child(imgnew);
+    imageRef.put(file)
+        .then(snapshot => {
+            return imageRef.getDownloadURL();
+            console.log('julieta 2');
+        })
+        
+        .then(async (downloadURL) => {
+          console.log('julieta');
+            URL = downloadURL;
+            await db.collection('tasks').doc().set({
+                title,
+                URL,
+                description
+            })
+        })
+}) 
